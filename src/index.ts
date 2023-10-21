886e9b69b5b3cfaa5603ccdf21649d16c6d1ae20
Joseph Bartley <josephdanielbartley@gmail.com>
2023-10-21 00:03:41 -0500

    refactor: added lint and prettier and refactored A TON to satisfy it

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,97 @@
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs'
 
-export interface PmfConfig {
-    delimiter: string
-    shape?: object
+export interface PmfConfig<T> {
+	delimiter: string
+	shape?: T
 }
 
-export function parseMyFile(filepath: string, config: PmfConfig) {
-    const fileExists = existsSync(filepath)
-    if (!fileExists) throw new Error(`Provided file path doesn't exist: ${filepath}`)
+export function parseMyFile<T>(filepath: string, config: PmfConfig<T>): T[] {
+	const fileExists = existsSync(filepath)
+	if (!fileExists)
+		throw new Error(`Provided file path doesn't exist: ${filepath}`)
 
-    const fileString = readFileSync(filepath).toString()
-    
-    const lines = fileString.split('\n')
-    const header = lines[0]
-    const headerSegments = header.split(config.delimiter).map((w: string) => w.trim())
-    let parseKey = {} as { [key: string]: number }
- 
-    if (config.shape) {
-        parseKey = buildParseKey(parseKey, config.shape, headerSegments)
-    } else {
-        headerSegments.forEach((hs, i) => {
-            parseKey[hs] = i
-        })
-    }
+	const fileString = readFileSync(filepath).toString()
 
-    const result = []
-    for (let i = 1; i < lines.length; ++i) {
-        const lineString = lines[i]
-        if (!lineString) continue
+	const lines = fileString.split('\n')
+	const header = lines[0]
+	const headerSegments = header
+		.split(config.delimiter)
+		.map((w: string) => w.trim())
+	let parseKey = {} as T
 
-        result.push(parseLine(lineString, parseKey, config))
-    }
+	if (config.shape) {
+		parseKey = buildParseKey(parseKey, config.shape, headerSegments)
+	} else {
+		headerSegments.forEach((hs, i) => {
+			Object.defineProperty(parseKey, hs, {
+				value: i,
+				enumerable: true,
+			})
+		})
+	}
 
-    return result
+	const result: T[] = []
+	for (let i = 1; i < lines.length; ++i) {
+		const lineString = lines[i]
+		if (!lineString) continue
+
+		result.push(parseLine(lineString, parseKey, config))
+	}
+
+	return result
 }
 
-function parseLine(lineString: string, parseKey: any, config: PmfConfig) {
-    const lineSegments = lineString.split(config.delimiter)
-
-    let result = Object.assign({}, parseKey)
-
-    const entries = Object.entries(parseKey)
-    for (let entry of entries) {
-        if (typeof(entry[1]) === 'object') {
-            result[entry[0]] = parseLine(lineString, entry[1], config)
-        } else {
-            result[entry[0]] = lineSegments[entry[1] as number].trim()
-        }
-    }
-
-    return result
+function buildParseKey<T, K>(
+	parseKey: K,
+	shape: T,
+	headerSegments: string[],
+): K {
+	for (const key in shape) {
+		const value = shape[key]
+		console.log(key, value)
+		if (typeof value === 'object') {
+			Object.defineProperty(parseKey, key, {
+				value: buildParseKey({}, value, headerSegments),
+				enumerable: true,
+			})
+		} else if (typeof value === 'string') {
+			Object.defineProperty(parseKey, key, {
+				value: headerSegments.indexOf(value),
+				enumerable: true,
+			})
+		} else {
+			throw new Error(
+				'Parse-My-file shape does not accept shape values that are not an object or a string',
+			)
+		}
+		console.log(parseKey)
+	}
+	return parseKey
 }
 
-function buildParseKey(parseKey: any, shape: object, headerSegments: string[]) {
-    const entries = Object.entries(shape!)
-    for (let entry of entries) {
-        if (typeof(entry[1]) === 'object') {
-            parseKey[entry[0]] = buildParseKey({}, entry[1], headerSegments)
-        } else {
-            parseKey[entry[0]] = headerSegments.indexOf(entry[1])
-        }
-    }
-    return parseKey
+function parseLine<T, K>(
+	lineString: string,
+	parseKey: K,
+	config: PmfConfig<T>,
+): K {
+	const lineSegments = lineString.split(config.delimiter)
+
+	console.log('parseLine', parseKey)
+	const result: K = Object.assign({}, parseKey)
+	for (const key in parseKey) {
+		const value = parseKey[key]
+		if (typeof value === 'object') {
+			Object.defineProperty(result, key, {
+				value: parseLine(lineString, value, config),
+				enumerable: true,
+			})
+		} else {
+			Object.defineProperty(result, key, {
+				value: lineSegments[value as number].trim(),
+				enumerable: true,
+			})
+		}
+	}
+
+	return result
 }
